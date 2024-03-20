ef148537fa4644f8094947c924f38581d7012f30
Yeani <ifeanyi200019@gmail.com>
2024-03-20 09:42:59 -0400

    new shop routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ require('./config/database');
 
 var indexRouter = require('./routes/index');
 var shopsRouter = require('./routes/shops');
+var reviewsRouter = require('./routes/reviews')
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/shops', shopsRouter);
+app.use('/', reviewsRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
